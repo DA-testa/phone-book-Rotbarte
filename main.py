3a9f60b0ef0e708498c2297584d1031c7d78a887
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-03 01:55:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 class Query:
+
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
+            
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
@@ -17,13 +18,17 @@ def process_queries(queries):
     contacts = {}
 
     for cur_query in queries:
+    
         if cur_query.type == 'add':
             contacts[cur_query.number] = cur_query.name
+            
         elif cur_query.type == 'del':
             contacts.pop(cur_query.number, None)
+            
         else:
             response = contacts.get(cur_query.number, 'not found')
             result.append(response)
+            
     return result
 
 if __name__ == '__main__':
